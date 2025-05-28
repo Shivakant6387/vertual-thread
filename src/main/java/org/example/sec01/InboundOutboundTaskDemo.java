be71ff0f209fd78991dba60fb1a0fdd2ec959223
shivakant singh <shivakant5875@gmail.com>
2025-05-28 15:17:33 +0530

    Use Thread currentThread Method

--- src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -4,7 +4,7 @@ import java.util.concurrent.CountDownLatch;
 
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
-    private static final int MAX_VIRTUAL = 50_000;
+    private static final int MAX_VIRTUAL = 20;
 
     public static void main(String[] args) throws InterruptedException {
 //        platFormThreadDemo3();
