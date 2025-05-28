957bcdc26bbbe64bbc44897dd997c953d0f42cc8
shivakant singh <shivakant5875@gmail.com>
2025-05-28 13:05:30 +0530

    CountDownLatch

--- src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/org/example/sec01/InboundOutboundTaskDemo.java
@@ -1,9 +1,11 @@
 package org.example.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
     private static final int MAX_PLATFORM = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         platFormThreadDemo3();
     }
 
@@ -24,13 +26,18 @@ public class InboundOutboundTaskDemo {
             thread.start();
         }
     }
-    private static void platFormThreadDemo3() {
-//        Thread.Builder.OfPlatform builder=Thread.ofPlatform().name("vins",1);
+
+    private static void platFormThreadDemo3() throws InterruptedException {
+        var latch = new CountDownLatch(MAX_PLATFORM);
         var builder = Thread.ofPlatform().name("daemon", 1);
         for (int i = 0; i < MAX_PLATFORM; i++) {
             int j = i;
-            Thread thread = builder.unstarted(() -> Task.ioIntensive(j));
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(j);
+                latch.countDown();
+            });
             thread.start();
         }
+        latch.await();
     }
 }
